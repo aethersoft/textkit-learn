75df0ff59f506cf81785b9f6726e75fcf1ea5f25
Yasas <wayasas@gmail.com>
2018-03-09 10:04:12 +0530

    add global_pooling 2D utility function

--- tklearn/utils/keras.py
+++ tklearn/utils/keras.py
@@ -1,4 +1,4 @@
-from keras.layers import GlobalAveragePooling1D, GlobalMaxPooling1D
+from keras.layers import GlobalAveragePooling1D, GlobalMaxPooling1D, GlobalMaxPooling2D, GlobalAveragePooling2D
 
 __all__ = [
     'global_pooling_1d'
@@ -16,5 +16,20 @@ def global_pooling_1d(t):
         return GlobalMaxPooling1D()
     elif t == 'avg':
         return GlobalAveragePooling1D()
+    else:
+        assert False, 'Unidentified pooling method named {}. Please use one of {}'.format(t, ['max', 'avg'])
+
+
+def global_pooling_2d(t):
+    """
+    Creates a 1D Global Pooling Layer
+
+    :param t: type of the pooling layer
+    :return: Max/Avg Pooling layer as specified by type param
+    """
+    if t == 'max':
+        return GlobalMaxPooling2D()
+    elif t == 'avg':
+        return GlobalAveragePooling2D()
     else:
         assert False, 'Unidentified pooling method named {}. Please use one of {}'.format(t, ['max', 'avg'])
