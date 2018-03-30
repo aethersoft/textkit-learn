7ebc44de4ff1205edcf349fc3032b17feafbefe3
Yasas Senarath <wayasas@gmail.com>
2018-03-30 18:11:47 +0530

    create and add keras classifier interface

--- tklearn/neural_network/KerasClassifier.py
+++ tklearn/neural_network/KerasClassifier.py
@@ -3,33 +3,78 @@ from abc import ABC, abstractmethod
 from sklearn.base import BaseEstimator, ClassifierMixin
 
 
-class KerasClassifier(ABC, BaseEstimator, ClassifierMixin):
-    def __init__(self):
-        """
-        Initialize the classifier
-        """
-        pass
+class KerasRegressor(ABC, BaseEstimator, ClassifierMixin):
+    """
+    An more abstract version of KerasClassifier for scikit-learn with custom preprocess pipeline.
+    """
 
-    def fit(self, Xs, y):
+    def __init__(self, batch_size=16, epochs=8):
         """
-        Fits the training data to the defined classifier
+        Initialize the Classifier.
 
-        :param X: Features
-        :param y: Labels
+        :param batch_size: Number of samples per gradient update. If unspecified, it will default to 16
+        :param epochs: Number of epochs to train the model. An epoch is an iteration over the entire x and y data provided.
+        """
+        self.batch_size = batch_size
+        self.epochs = epochs
+
+    def fit(self, X, y=None):
+        """
+        Fit the model according to the given training data
+
+        :param X: Training vector with  shape = [n_samples, n_features],
+         where n_samples in the number of samples and  n_features is the number of features.
+        :param y: Target vector relative to X of shape shape = [n_samples]
         :return: self
         """
-        raise NotImplementedError
+        X, y = self.preprocess(X, y)
+        self.model_ = self.build_model(X, y)
+        self.model_.fit(X, y, batch_size=self.batch_size, epochs=self.epochs)
+        return self
 
-    def predict(self, X, y=None, *args, **kwargs):
+    def predict(self, X):
         """
-        Predicts the classes and return them
-        :param X: Feature matrix [n_samples, num_features]
-        :param y: None
-        :param args: Extra args
-        :param kwargs: Extra kwargs
-        :return: predicted classes
+        Perform classification on samples in X / Predict target values of X given a model
+
+        :param X: Testing vector
+        :return: Predicted values
         """
-        raise NotImplementedError
+        X, _ = self.preprocess(X)
+        return self.model_.predict(X)
+
+    def score(self, X, y, sample_weight=None):
+        """
+        Returns the accuracy on the given test data and labels.
+
+        :param X: Test samples
+        :param y: True labels for X
+        :param sample_weight: Sample weights
+        :return:
+        """
+        _, acc = self.evaluate(X, y, sample_weight)
+        return acc
+
+    def evaluate(self, x_train, y, sample_weight=None):
+        """
+        Returns the (loss, accuracy) tuple on the given test data and labels.
+
+        :param x_train: Test samples with  shape = [n_samples, n_features],
+         where n_samples in the number of samples and  n_features is the number of features.
+        :param y: True labels for X of shape shape = [n_samples]
+        :param sample_weight: Sample weights.
+        :return:
+        """
+        x_train = self.preprocess(x_train)
+        return self.model_.evaluate(x_train, y, batch_size=self.batch_size)
+
+    @abstractmethod
+    def build_model(self, X, y):
+        """
+        Compiles the CNN model
+
+        :return: compiled model
+        """
+        pass
 
     @abstractmethod
     def save(self, filepath):
@@ -48,6 +93,15 @@ class KerasClassifier(ABC, BaseEstimator, ClassifierMixin):
         :return: Saved model
         """
         raise NotImplementedError
+
+    @abstractmethod
+    def preprocess(self, X, y=None):
+        """
+        Preprocess the data and return the preprocessed input for the model
+
+        :return: model input
+        """
+        return X, y
 
     def __getstate__(self):
         return {}
