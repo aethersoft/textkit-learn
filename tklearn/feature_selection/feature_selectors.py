a8a993da1de798428e072a89b7f97d1f114d3ef0
Yasas Senarath <wayasas@gmail.com>
2018-04-03 13:36:09 +0530

    update selec by index to support axis 1 or 2

--- tklearn/feature_selection/feature_selectors.py
+++ tklearn/feature_selection/feature_selectors.py
@@ -2,18 +2,23 @@ from sklearn.preprocessing import FunctionTransformer
 
 
 class SelectByIndex(FunctionTransformer):
-    def __init__(self, index):
+    def __init__(self, index, axis=1):
         """
         Given index selects the feature(s) indicated by that index. The input to the fit function should support __getitem__.
 
         :param index: a index to select
+        :type axis: axis
         """
-        super(SelectByIndex, self).__init__(self._select_by_getitem_func, validate=False, kw_args={'item': index})
+        super(SelectByIndex, self).__init__(self._select_by_getitem_func, validate=False)
+        self.index = index
+        self.axis = axis
 
-    @staticmethod
-    def _select_by_getitem_func(X, item):
+    def _select_by_getitem_func(self, X):
         assert hasattr(X, '__getitem__'), '\'{}\' object is not subscriptable.'.format(type(X))
-        return X[item]
+        assert 0 < self.axis < 3, 'SelectByIndex supports only axis 1 or 2'
+        if self.axis == 2:
+            return list(zip(*X))[self.index]
+        return X[self.index]
 
 
 class SelectByKey(SelectByIndex):
@@ -23,4 +28,4 @@ class SelectByKey(SelectByIndex):
 
         :param key: a key to select
         """
-        super(SelectByKey, self).__init__(key)
+        super(SelectByKey, self).__init__(key, 1)
