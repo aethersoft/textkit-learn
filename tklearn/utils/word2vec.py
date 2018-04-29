14245996d899ae819c512129b59c4b74d48548cd
Yasas <wayasas@gmail.com>
2018-04-29 21:01:00 +0530

    update word2vec with dummy proxy objectthat do a shallow copy on deepcopy

--- tklearn/utils/word2vec.py
+++ tklearn/utils/word2vec.py
@@ -5,6 +5,26 @@ __all__ = [
 ]
 
 
+class Word2Vec:
+    def __init__(self, w2v, binary=True, unicode_errors='ignore'):
+        self.w2v = w2v
+
+    @property
+    def vector_size(self):
+        return self.w2v.vector_size
+
+    def __getitem__(self, item):
+        return self.w2v[item]
+
+    def __deepcopy__(self, memodict={}):
+        obj = Word2Vec(self.w2v)
+        return obj
+
+    def __copy__(self):
+        obj = Word2Vec(self.w2v)
+        return obj
+
+
 def load_word2vec(path, binary=True, unicode_errors='ignore'):
     """
     Loads and returns word2vec
@@ -14,4 +34,5 @@ def load_word2vec(path, binary=True, unicode_errors='ignore'):
     :param path: path to word2vec file
     :return: Binary Word2Vec
     """
-    return KeyedVectors.load_word2vec_format(path, binary=True, unicode_errors='ignore')
+    w2v = KeyedVectors.load_word2vec_format(path, binary=binary, unicode_errors=unicode_errors)
+    return Word2Vec(w2v, binary=binary, unicode_errors=unicode_errors)
