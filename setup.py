2300d174b0ff4facae5541380b21c3d32b97cf12
Yasas <wayasas@gmail.com>
2018-05-16 15:27:01 +0530

    add setup file

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(name='tklearn',
+      version='0.1',
+      description='A Natural Language Learning Toolkit',
+      url='https://github.com/aethersoft/textkit-learn',
+      author='Yasas Senarath',
+      author_email='wayasas@gmail.com',
+      license='MIT',
+      packages=find_packages(),
+      package_dir={'tklearn': 'tklearn'},
+      zip_safe=False)
