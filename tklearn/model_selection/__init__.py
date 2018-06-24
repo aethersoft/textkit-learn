97f52b658310f649d9b350f0d06f29eadb8d05d9
Yasas <wayasas@gmail.com>
2018-06-24 20:20:59 +0530

    add hyperopt support

--- tklearn/model_selection/__init__.py
+++ tklearn/model_selection/__init__.py
@@ -1,3 +1,4 @@
+from ._search import HyperoptOptimizer
 from ._validation import multitask_cross_val_predict
 
 __all__ = [
---
+++ tklearn/model_selection/_search.py
@@ -0,0 +1,88 @@
+import logging
+import time
+
+from hyperopt import hp, tpe, fmin, Trials, STATUS_OK, rand
+
+logger = logging.getLogger(__name__)
+
+__all__ = [
+    'HyperoptOptimizer'
+]
+
+
+def _build_space(specs, name=''):
+    if specs is None:
+        raise TypeError('Invalid search space specification.')
+    val_type = specs['type']
+    val_val = specs['value'] if hasattr(specs, 'value') else None
+    if val_type == 'rlist':
+        length = specs['length']
+        val = []
+        for j in range(length[0], length[1] + 1, 1):
+            lst = []
+            for i in range(j):
+                lst.append(_build_space(val_val, '{}_{}_{}'.format(name, j, i)))
+            val.append(lst)
+        return hp.choice(name, val)
+    elif val_type == 'dict':
+        val = {}
+        for k, v in val_val.items():
+            val[k] = _build_space(v, name=k)
+        return val
+    elif val_type == 'list':
+        val = []
+        for i, item in enumerate(val_val):
+            val.append(_build_space(item, name='{}_{}'.format(name, i)))
+        return val
+    elif val_type == 'choice':
+        val = []
+        for i, v in enumerate(val_val):
+            val.append(_build_space(v, name='{}_{}'.format(name, i)))
+        return hp.choice(name, val)
+    elif val_type == 'uniform':
+        if len(val_val) == 2:
+            return hp.uniform(name, val_val[0], val_val[1])
+        elif len(val_val) == 3:
+            return hp.quniform(name, val_val[0], val_val[1], val_val[2])
+    elif val_type == 'var':
+        return val_val
+    else:
+        logger.warning('Ignoring invalid value type and continuing.')
+    return []
+
+
+class HyperoptOptimizer:
+    def __init__(self, estimator, param_dist, scorer, max_evals=10, search_algorithm='tpe'):
+        """
+        Initialize Hyperopt Optimizer object.
+        :param estimator: Estimator object; A object of that type is instantiated for each evaluation point.
+        :param param_dist: Description of the parameters
+        :param max_evals: Allow up to this many function evaluations before returning.
+        """
+        self.estimator = estimator
+        self.param_dist = param_dist
+        self.scorer = scorer
+        self.max_evals = max_evals
+        self.search_algorithm = search_algorithm
+
+    def optimize(self, X_train, X_test, y_train, y_test):
+        trials = Trials()
+        space = _build_space({
+            'type': 'dict',
+            'value': self.param_dist
+        })
+
+        def objective(kwargs):
+            y_pred = self.estimator(**kwargs).fit(X_train, y_train).predict(X_test)
+            loss = -self.scorer(y_test, y_pred)
+            return {
+                'loss': loss,
+                'status': STATUS_OK,
+                'eval_time': time.time(),
+            }
+
+        if self.search_algorithm == 'rand':
+            algo = rand.suggest
+        else:
+            algo = tpe.suggest
+        return fmin(objective, space=space, algo=algo, max_evals=self.max_evals, trials=trials)
