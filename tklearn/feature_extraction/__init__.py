642f34bb0f8c8d7a8bbda60a9070de8da741d394
Yasas <wayasas@gmail.com>
2018-06-25 23:51:46 +0530

    remove LexiconFeaturizer from package

--- tklearn/feature_extraction/__init__.py
+++ tklearn/feature_extraction/__init__.py
@@ -1,13 +1,11 @@
 from .affective_tweets import AffectiveTweetsVectorizer
 from .embedding_averager import EmbeddingAverager
 from .embedding_extractor import EmbeddingExtractor
-from .lexicon_featurizer import LexiconFeaturizer
 from .transfer_featurizer import TransferFeaturizer
 
 __all__ = [
     'EmbeddingAverager',
     'EmbeddingExtractor',
-    'LexiconFeaturizer',
     'AffectiveTweetsVectorizer',
     'TransferFeaturizer',
 ]
