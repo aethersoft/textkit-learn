27cd1970fb2529ba908a2b65c0d9e51e18fb6e0f
Yasas <wayasas@gmail.com>
2018-06-27 17:28:38 +0530

    remove embedding extractor and averager since it has been ported to embedding_transformer

--- tklearn/feature_extraction/__init__.py
+++ tklearn/feature_extraction/__init__.py
@@ -1,11 +1,8 @@
 from .affective_tweets import AffectiveTweetsVectorizer
-from .embedding_averager import EmbeddingAverager
-from .embedding_extractor import EmbeddingExtractor
+from .embedding_transformer import EmbeddingTransformer
 from .transfer_featurizer import TransferFeaturizer
 
 __all__ = [
-    'EmbeddingAverager',
-    'EmbeddingExtractor',
     'AffectiveTweetsVectorizer',
     'TransferFeaturizer',
 ]
