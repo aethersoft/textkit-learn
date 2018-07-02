117ef9475d4bd7607c1bb5c111f7e38c8826dbf7
Yasas <wayasas@gmail.com>
2018-07-02 19:48:31 +0530

    add test set support

--- tklearn/datasets/load_iest.py
+++ tklearn/datasets/load_iest.py
@@ -5,12 +5,15 @@ def load_iest(path):
     train_path = os.path.join(path, 'train.csv')
     trial_path = os.path.join(path, 'trial.csv')
     trial_label_path = os.path.join(path, 'trial.labels')
+    test_path = os.path.join(path, 'test.csv')
     with open(train_path, 'r', encoding='utf-8') as train_file, \
             open(trial_path, 'r', encoding='utf-8') as trial_file, \
-            open(trial_label_path, 'r', encoding='utf-8') as trial_label_file:
+            open(trial_label_path, 'r', encoding='utf-8') as trial_label_file, \
+            open(test_path, 'r', encoding='utf-8') as test_file:
         train_csv = [t.split('\t') for t in train_file.read().strip().split('\n')]
         trial_csv = [t.split('\t')[1] for t in trial_file.read().strip().split('\n')]
         trial_labels = trial_label_file.read().strip().split('\n')
         assert len(trial_labels) == len(trial_csv), 'Invalid trial file contents.'
         trial_csv = [[l, t] for l, t in zip(trial_labels, trial_csv)]
-    return train_csv, trial_csv
+        test_csv = [t.split('\t') for t in test_file.read().strip().split('\n')]
+    return train_csv, trial_csv, test_csv
