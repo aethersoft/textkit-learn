1dc45d2e4a248841bdc8857f88cd6c9314a97ad3
Yasas <wayasas@gmail.com>
2018-08-24 09:37:34 +0530

    remove packages and add related modules.

--- tklearn/text/embedding/__init__.py
+++
@@ -1,9 +0,0 @@
-from .word_embedding import load_glove
-from .word_embedding import load_word2vec
-from .word_embedding import load_embedding
-
-__all__ = [
-    'load_glove',
-    'load_word2vec',
-    'load_embedding',
-]
