dec9f6ee38d538308ae0062bd51d8cadd15f17ae
Yasas <wayasas@gmail.com>
2018-11-12 09:10:35 +0530

    Change var name of validation data

--- tklearn/utils/keras.py
+++ tklearn/utils/keras.py
@@ -5,17 +5,15 @@ from sklearn.metrics import f1_score, accuracy_score
 class ValidationLogger(Callback):
     def __init__(self, validation_data, scorer='accuracy'):
         super(ValidationLogger, self).__init__()
-        self.validation_data = validation_data
-        self.scorer = scorer
+        self._validation_data = validation_data
+        self._scorer = scorer
 
     def on_epoch_end(self, epoch, logs=None):
-        if logs is None:
-            logs = {}
-        x, y = self.validation_data
-        if self.scorer == 'f1.micro':
+        x, y = self._validation_data
+        if self._scorer == 'f1.micro':
             scorer = lambda y_true, y_pred: f1_score(y_true, y_pred, average='macro')
         else:
             scorer = lambda y_true, y_pred: accuracy_score(y_true, y_pred)
         y_pred = self.model.predict(x)
         score = scorer(y, y_pred)
-        print('\nTesting {}: {}\n'.format(self.scorer, score))
+        print('\nTesting {}: {}\n'.format(self._scorer, score))
