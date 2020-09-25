e1b544883dea96a1687d8ecea90d7b5531d37393
Yasas Senarath <ysenarath.93@gmail.com>
2020-09-25 13:28:16 -0400

    Update version to v0.0.4.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="tklearn",  # Replace with your own username
-    version="v0.0.3",
+    version="v0.0.4",
     author="Yasas Senarath",
     description="Helps computers to understand human languages.",
     long_description=long_description,
