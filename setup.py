2997540edb752c15e4a1acd0bf80867a9eb0c173
Yasas <ysenarath.93@gmail.com>
2021-04-19 21:36:00 -0400

    Update version information

--- setup.py
+++ setup.py
@@ -1,19 +1,42 @@
 import setuptools
+import codecs
+import os.path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
+with open('packages.txt') as fh:
+    requirements = fh.read().strip().split('\n')
+
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
+
+
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
+
+
 setuptools.setup(
     name="textkit-learn",
-    version="v0.0.5",
+    version="0.0.5",
     author="Yasas Senarath",
     description="Helps computers to understand human languages.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/ysenarath/textkit-learn",
     packages=setuptools.find_packages(),
+    install_requires=requirements,
     classifiers=[
         "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.6',
