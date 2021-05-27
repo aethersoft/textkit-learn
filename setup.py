83c4dd6d6437a116b0096844df491b7ecc31e27d
Yasas <ysenarath.93@gmail.com>
2021-05-27 11:26:12 -0400

    Fix requirements path

--- setup.py
+++ setup.py
@@ -1,28 +1,10 @@
 import setuptools
-import codecs
-import os.path
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-with open('packages.txt') as fh:
+with open('requirements.txt') as fh:
     requirements = fh.read().strip().split('\n')
-
-
-def read(rel_path):
-    here = os.path.abspath(os.path.dirname(__file__))
-    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
-        return fp.read()
-
-
-def get_version(rel_path):
-    for line in read(rel_path).splitlines():
-        if line.startswith('__version__'):
-            delim = '"' if '"' in line else "'"
-            return line.split(delim)[1]
-    else:
-        raise RuntimeError("Unable to find version string.")
-
 
 setuptools.setup(
     name="textkit-learn",
